6b81c64c67b1ff20792728a5927cd321c38e993e
Zack <zack@zackbradshaw.com>
2024-06-05 12:04:32 -0500

    chore: fix deps

--- Plugins/blupy/Source/blupy/blupy.Build.cs
+++ Plugins/blupy/Source/blupy/blupy.Build.cs
@@ -4,62 +4,58 @@ using UnrealBuildTool;
 
 public class blupy : ModuleRules
 {
-	public blupy(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"EditorFramework",
-				"EditorStyle",
-				"UnrealEd",
-				"LevelEditor",
-				"InteractiveToolsFramework",
-				"EditorInteractiveToolsFramework"
-				"Core",
-				"CoreUObject",
-				"Engine",
-				"Slate",
-				"SlateCore",
-				"InputCore",
-				"HTTP",
-				"Json",
-				"JsonUtilities",
-				"ImageWrapper"
-				
-			}
-			);
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+    public blupy(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        
+        PublicIncludePaths.AddRange(
+            new string[] {
+                // ... add public include paths required here ...
+            }
+        );
+                
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                // ... add other private include paths required here ...
+            }
+        );
+            
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "Engine",
+                "InputCore",
+                "HTTP",
+                "Json",
+                "JsonUtilities",
+                "ImageWrapper"
+                // ... add other public dependencies that you statically link with here ...
+            }
+        );
+        
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "EditorFramework",
+                "EditorStyle",
+                "UnrealEd",
+                "LevelEditor",
+                "InteractiveToolsFramework",
+                "EditorInteractiveToolsFramework",
+                "Slate",
+                "SlateCore"
+                // ... add other private dependencies here ...
+            }
+        );
+        
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+                // ... add any modules that your module loads dynamically here ...
+            }
+        );
+    }
 }
+
