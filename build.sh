c8f401d002806306db8de1ac0e6ce1275cdd1c94
Zack <zack@zackbradshaw.com>
2024-06-14 23:46:57 -0500

    feat: vscode build script

---
+++ build.sh
@@ -0,0 +1,2 @@
+echo "Building Unreal Engine plugin"
+"../../../mnt/c/Program Files/Epic Games/UE_5.3/Engine/Build/BatchFiles/RunUAT.bat" BuildPlugin -Plugin="./Plugins/blupy/blupy.uplugin" -Package="./Plugins/blupy" -Rocket
